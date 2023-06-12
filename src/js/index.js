86767d62cc79bc66fa8b4618a48af9081ef01952
Dan <veras_dan@outlook.com>
2023-06-11 23:07:48 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,17 @@
 
 
 const botaoAterarTema = document.getElementById("botao-alterar-tema");
-
 const body = document.querySelector("body");
-
-const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao")
+const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao");
 
 botaoAterarTema.addEventListener("click", () => {
-    body.classList.add("modo-escuro");
-    imagemBotaoTrocaDeTema.setAttribute("src", "src/images/moon.png");
+    const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
 
-    
-});
+    if (modoEscuroEstaAtivo) {
+        body.classList.remove("modo-escuro");
+        imagemBotaoTrocaDeTema.setAttribute("src", "src/images/sun.png");
+    } else {
+            body.classList.add("modo-escuro");
+            imagemBotaoTrocaDeTema.setAttribute("src", "src/images/moon.png");
+        };
+    });
